356fbc5a650bdc18e505ca48232b7d525081da9b
Your Name <suhasmashety@gmail.com>
2025-10-06 16:20:24 +0000

    added project routes

---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import projectRoutes from "./routes/projectsRoutes"
+
+
+dotenv.config()
+const app = express();
+app.use(express.json())
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy( { policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+// {Routes}
+app.get("/", (req, res) => {
+    res.send("This is home route");
+})
+
+app.use("/Projects", projectRoutes)
+
+
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on part ${port}`);
+});
+
+
+
