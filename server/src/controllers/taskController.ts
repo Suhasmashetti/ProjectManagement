59662a8f26cfe5035122467272be6df88399126d
Your Name <suhasmashety@gmail.com>
2025-10-07 13:59:06 +0000

    added taskcontroller

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -55,3 +55,41 @@ export const getTasks = async( req: Request, res: Response ): Promise<void> => {
     }
 };
 
+export const updateTaskStatus = async (req: Request, res: Response): Promise<void> => {
+    const { taskId } = req.params;
+    const { status } = req.body;
+    try{
+        const updatedTask = await prisma.task.update({
+            where: {
+                id: Number(taskId),
+            },
+            data: {
+                status: status,
+            },
+        });
+        res.json(updatedTask);
+    } catch (error: any) {
+        res.status(500).json({message:`Erro updating Task ${error.message}`});
+    }
+};
+
+export const getUserTasks = async(req: Request, res: Response): Promise<void> => {
+    const {userId} = req.params;
+    try {
+        const tasks = await prisma.task.findMany({
+            where: {
+                OR: [
+                    {authorUserId: Number(userId)},
+                    {assignedUserId: Number(userId)},
+                ]
+            },
+            include: {
+                author: true,
+                assignee: true,
+            },
+        });
+        res.json(tasks);
+    } catch (error: any) {
+        res.status(500).json({message:`Error fetching tasks ${error.message}`});
+    }
+}
