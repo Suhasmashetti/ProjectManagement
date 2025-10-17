8dea0968bf8152227ccb11a848ee4145726c1e88
Your Name <suhasmashety@gmail.com>
2025-10-17 14:43:29 +0000

    updated

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -2,20 +2,32 @@
 
 import React, { useState } from 'react'
 import ProjectHeader from '../ProjectHeader'
+import BoardView from '../BoardView'
+import ListView from '../ListView'
+import Timeline from '../Timeline'
 
 type Props = {
     params: {id: string}; 
 }
 
 const Project = ({params}: Props) => {
-    const id = params;
+    const {id} = params;
     const [activeTab, setActiveTab ] = useState("Board");
-    const [isModalNewTaskOpen, setIsModelNewTaskOpen ] = useState(false);
+    const [isModalNewTaskOpen, setIsModalNewTaskOpen ] = useState(false);
 
   return (
 //   {MODAL NEW TASKS}
     <div>
         <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+        { activeTab === "Board" && (
+          <BoardView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen}/>
+        )}
+        { activeTab === "List" && (
+          <ListView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen}/>
+        )}
+        { activeTab === "Timeline" && (
+          <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen}/>
+        )}
     </div>
   )
 }
